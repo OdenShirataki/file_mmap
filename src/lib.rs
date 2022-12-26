b2312a3e80f29fc250353051038cf9509aead99e
Oden <oden.srtk@gmail.com>
2022-12-26 22:17:02 +0900

    delete FileMmap.len.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use memmap2::MmapRaw;
 use std::{
-    fs::{File, OpenOptions},
-    io::{Seek, SeekFrom},
+    fs,
+    io::{self, Seek},
     mem::ManuallyDrop,
+    ptr,
 };
 
 pub struct FileMmap {
-    file: File,
+    file: fs::File,
     mmap: ManuallyDrop<Box<MmapRaw>>,
-    len: u64,
 }
 
 impl Drop for FileMmap {
@@ -18,27 +18,18 @@ impl Drop for FileMmap {
 }
 
 impl FileMmap {
-    pub fn new(path: &str, inital_size: u64) -> Result<Self, std::io::Error> {
-        let mut file = OpenOptions::new()
+    pub fn new(path: &str) -> io::Result<Self> {
+        let mut file = fs::OpenOptions::new()
             .read(true)
             .write(true)
             .create(true)
             .open(&path)?;
-        let mut len = file.metadata()?.len();
-        if len == 0 {
-            file.set_len(if inital_size == 0 {
-                1 //If the size is 0, it seems to fail, so if it is 0, specify 1 byte for the time being
-            } else {
-                inital_size
-            })?;
-            len = inital_size;
-        }
-        file.seek(SeekFrom::Start(len))?;
+        file.seek(io::SeekFrom::End(0))?;
         let mmap = ManuallyDrop::new(Box::new(MmapRaw::map_raw(&file)?));
-        Ok(FileMmap { file, mmap, len })
+        Ok(FileMmap { file, mmap })
     }
-    pub fn len(&self) -> u64 {
-        self.len
+    pub fn len(&self) -> io::Result<u64> {
+        Ok(self.file.metadata()?.len())
     }
     pub fn as_ptr(&self) -> *const u8 {
         self.mmap.as_ptr()
@@ -49,35 +40,34 @@ impl FileMmap {
     pub unsafe fn bytes(&self, addr: isize, len: usize) -> &[u8] {
         std::slice::from_raw_parts(self.mmap.as_ptr().offset(addr), len)
     }
-    pub fn set_len(&mut self, len: u64) -> std::io::Result<()> {
+    pub fn set_len(&mut self, len: u64) -> io::Result<()> {
         let current_len = self.file.metadata()?.len();
-        if len > current_len {
+        if len > current_len && current_len > 0 {
             self.file.set_len(len)?;
         } else {
             unsafe { ManuallyDrop::drop(&mut self.mmap) };
             self.file.set_len(len)?;
             self.mmap = ManuallyDrop::new(Box::new(MmapRaw::map_raw(&self.file).unwrap()));
         }
-        self.len = len;
         Ok(())
     }
-    pub fn append(&mut self, bytes: &[u8]) -> Result<u64, std::io::Error> {
-        let addr = self.len;
-        self.set_len(self.len + bytes.len() as u64)?;
+    pub fn append(&mut self, bytes: &[u8]) -> io::Result<u64> {
+        let addr = self.file.metadata()?.len();
+        self.set_len(addr + bytes.len() as u64)?;
         unsafe {
-            self.write(addr, bytes);
+            self.write(addr as isize, bytes);
         }
         Ok(addr)
     }
-    pub unsafe fn write(&mut self, addr: u64, bytes: &[u8]) {
+    pub unsafe fn write(&mut self, addr: isize, bytes: &[u8]) {
         let len = bytes.len();
-        std::ptr::copy(
+        ptr::copy(
             bytes.as_ptr(),
-            self.mmap.as_ptr().offset(addr as isize) as *mut u8,
+            self.mmap.as_ptr().offset(addr) as *mut u8,
             len,
         );
     }
-    pub unsafe fn write_0(&mut self, addr: isize, len: u64) {
-        std::ptr::write_bytes(self.mmap.as_ptr().offset(addr) as *mut u8, 0, len as usize);
+    pub unsafe fn write_0(&mut self, addr: isize, len: usize) {
+        ptr::write_bytes(self.mmap.as_ptr().offset(addr) as *mut u8, 0, len);
     }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -3,6 +3,6 @@ use file_mmap::FileMmap;
 #[test]
 fn test(){
     let path="D:\\test.data";
-    let mut filemmap=FileMmap::new(path,0).unwrap();
+    let mut filemmap=FileMmap::new(path).unwrap();
     filemmap.append(b"test").unwrap();
 }
